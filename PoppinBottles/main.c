0fa7a0c0cfd11fb72bbb9668d524362038a34b5f
Jeremy Petter <jeremy.petter@gmail.com>
2015-05-05 23:49:15 -0700

    initial commit with customer struct definition and README

---
+++ PoppinBottles/main.c
@@ -0,0 +1,25 @@
+//
+//  main.c
+//  PoppinBottles
+//
+//  Created by Jeremy Petter on 2015-05-05.
+//  Copyright (c) 2015 Jeremy Petter. All rights reserved.
+//
+
+#include <stdio.h>
+
+struct Customer {
+    int dollars;
+    int bottles;
+    int caps;
+};
+
+typedef struct Customer Customer;
+
+int main(int argc, const char * argv[]) {
+    
+    
+    
+    
+    return 0;
+}
